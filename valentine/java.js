5b6b2ea589f8b06258319ad4ab33f1ef58f46081
Mingz29 <133098957+Mingz29@users.noreply.github.com>
2024-02-13 23:49:50 +0800

    Add files via upload

---
+++ valentine/java.js
@@ -0,0 +1,27 @@
+var page = document.querySelectorAll(".details");
+var btn = document.querySelectorAll("button");
+var book = document.querySelector(".card");
+
+var place = 0;
+
+for(var i = 0; i < btn.length; i++){
+    place = i;
+    btn[i].addEventListener("click", function(){
+        page[place].style.transform = "rotateY(-135deg)";
+    });
+}
+
+book.addEventListener("mouseleave", function(){
+    for(var i = 0; i < page.length; i++){
+        place = i;
+        page[i].style.transform = "";
+    }   
+});
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', function() {
+    card.classList.toggle('is-flipped');
+  });
+});
